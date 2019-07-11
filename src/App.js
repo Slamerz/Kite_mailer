a17da6ae12c15d8c950e4e12f17e9cb0a4ef1bc5
Unknown <keybladeschoosen1@gmail.com>
2019-07-11 10:48:11 -0400

    updated app.js routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import ManageOrders from "./containers/ManageOrders";
 import { Route, Switch } from "react-router";
 import NavBar from "./components/NavBar";
+import MessageForm from "./containers/MessageForm";
 
 function App() {
   return (
     <Switch>
       <Route path="/" exact component={NavBar} />
+      <Route path="/send" exact component={MessageForm} />
       <Route path="/orders" exact component={ManageOrders} />
       <Route path="/orders/:id" component={ManageOrders} />
     </Switch>
