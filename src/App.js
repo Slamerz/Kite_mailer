7356f666b76815b2fcfb2b01f97e64f86d2318ef
Unknown <keybladeschoosen1@gmail.com>
2019-07-12 13:19:15 -0400

    Update app.js
    added routs for the register page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import ManageOrders from "./containers/ManageOrders";
 import {Route, Switch} from "react-router";
 import NavBar from "./components/NavBar";
+import RegisterPage from "./containers/RegisterPage";
 
 function App() {
   return (
     <Switch>
         <Route path="/" exact component={NavBar}/>
-        <Route path="/orders/" exact component={ManageOrders}/>
+        <Route path="/nav" component={NavBar} />
+        <Route path="/register" exact component={RegisterPage}/>
+        <Route path="/orders" exact component={ManageOrders}/>
         <Route path="/orders/:id" component={ManageOrders}/>
     </Switch>
   );
