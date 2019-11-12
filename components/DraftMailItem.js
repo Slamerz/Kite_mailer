1b5b06d76f5d413cdd8022ded6837ad10686fa85
Zachary Kline <zkline888@gmail.com>
2019-11-12 08:45:05 -0500

    Committing three skeleton components for Draft Mail item, Sent Mail Item, and expanded sent mail

---
+++ components/DraftMailItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Header } from 'native-base'
+
+const DraftMailItem = props => {
+    return (
+        <Header>Filler</Header>
+    )
+}
+
+export default DraftMailItem
